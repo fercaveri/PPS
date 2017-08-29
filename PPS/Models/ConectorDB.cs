5d91e64948eea6ee7d608e10a32994edfa4fdc9f
fercaveri <ferc4@hotmail.com>
2017-08-28 23:32:49 -0300

    Pasado a net core 2.0 y agregado un contexto

---
+++ PPS/Models/ConectorDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PPS.Models
+{
+    public class ConectorDB : DbContext
+    {
+        public ConectorDB(DbContextOptions<ConectorDB> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Candidato> Candidatos { get; set; }
+    }
+}
