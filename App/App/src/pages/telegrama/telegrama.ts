59be9f0e1e451e8a9443819fabd4a54e00fec76a
Gaston <gastonsturla01@gmail.com>
2017-09-25 14:04:55 -0300

    Mini fix en la muestra de candidatos, ahora muestra por localidad los concejales seugn corresponda

--- App/App/src/pages/telegrama/telegrama.ts
+++ App/App/src/pages/telegrama/telegrama.ts
@@ -17,6 +17,7 @@ export class TelegramaPage{
     constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
         this.mesa = navParams.get('mesa');
         this.localidad = navParams.get('localidad');
+        console.log(this.localidad);
         this.getPartidos();
         this.getCandidatos();
     }
@@ -40,7 +41,7 @@ export class TelegramaPage{
         });
     }
     getCandidatos() {
-        this.http.get('http://localhost:59473/api/candidato' + this.localidad + '/').map(res => res.json()).subscribe(data => {
+        this.http.get('http://localhost:59473/api/candidato/' + this.localidad + '/').map(res => res.json()).subscribe(data => {
             this.candidatos = data;
             console.log(this.candidatos);
         });
