4ac880583b060aee9924312d0394998be919200a
fercaveri <ferc4@hotmail.com>
2017-10-02 00:14:31 -0300

    Cambiado el get de usuario y agregado 1 usuario superadmin

--- PPS/Controllers/UsuarioController.cs
+++ PPS/Controllers/UsuarioController.cs
@@ -13,31 +13,38 @@ namespace PPS.Controllers
 {
   [Route("api/[controller]")]
   public class UsuarioController
+  {
+    private ConectorDB _db;
+    public UsuarioController(ConectorDB db)
     {
-        private ConectorDB _db;
-        public UsuarioController (ConectorDB db)
-        {
-          _db= db;
-        }
-        // GET api/usuario
-        [HttpGet]
-        public IEnumerable<Usuario> Get(String usuario, String pass)
-        {
-            var usuarios = _db.Usuarios.Select(x => new Usuario(x.usuario, x.contraseña, x.nombreCompleto, (int)x.rol)).Where(x => x.usuario == usuario && x.contraseña == pass).ToList();
-            return usuarios;
-        }
+      _db = db;
+    }
+    // GET api/usuario
+    [HttpGet]
+    public bool Get(String usuario, String pass)
+    {
+      var usuarios = _db.Usuarios.Select(x => new Usuario(x.usuario, x.contraseña, x.nombreCompleto, (int)x.rol)).Where(x => x.usuario == usuario && x.contraseña == pass).ToList();
+      if (usuarios.Any())
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
 
-        [HttpPost]
-        public HttpResponseMessage Post([FromBody] UsuarioWEB user)
-        {
-          var usuario = _db.Usuarios.Select(x => new Usuario(x.usuario, x.contraseña, x.nombreCompleto, (int)x.rol)).Where(x => x.usuario == user.user && x.contraseña == user.pass).FirstOrDefault();
-          if (usuario == null)
-          {
-            _db.Usuarios.Add(new Usuario(user.user, user.pass, user.fullName, user.role));
-            _db.SaveChanges();
-            return new HttpResponseMessage(HttpStatusCode.OK);
-          }
-          return new HttpResponseMessage(HttpStatusCode.NotAcceptable);
-        }
+    [HttpPost]
+    public HttpResponseMessage Post([FromBody] UsuarioWEB user)
+    {
+      var usuario = _db.Usuarios.Select(x => new Usuario(x.usuario, x.contraseña, x.nombreCompleto, (int)x.rol)).Where(x => x.usuario == user.user).FirstOrDefault();
+      if (usuario == null)
+      {
+        _db.Usuarios.Add(new Usuario(user.user, user.pass, user.fullName, user.role));
+        _db.SaveChanges();
+        return new HttpResponseMessage(HttpStatusCode.OK);
+      }
+      return new HttpResponseMessage(HttpStatusCode.NotAcceptable);
+    }
   }
 }
--- PPS/Data/DbInitializer.cs
+++ PPS/Data/DbInitializer.cs
@@ -9,67 +9,67 @@ namespace PPS.Data
   public class DbInitializer
   {
 
-        public static void Initialize(ConectorDB context)
+    public static void Initialize(ConectorDB context)
+    {
+      //context.Database.EnsureDeleted();
+      context.Database.EnsureCreated();
+
+      Provincia provinciaBsAs = new Provincia("Buenos Aires");
+      Provincia provinciaCba = new Provincia("Cordoba");
+      Provincia provinciaSalta = new Provincia("Salta");
+      Provincia provinciaCorrientes = new Provincia("Corrientes");
+      Provincia nacional = new Provincia("Nacional");
+
+      PartidoPolitico pro = new PartidoPolitico("Pro", provinciaBsAs);
+      var provincias = new Provincia[] { provinciaBsAs, provinciaCba, provinciaSalta, provinciaCorrientes, nacional };
+      if (!context.Provincias.Any())
+      {
+
+        foreach (Provincia p in provincias)
         {
-            //context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-      
-            Provincia provinciaBsAs = new Provincia("Buenos Aires");
-            Provincia provinciaCba = new Provincia("Cordoba");
-            Provincia provinciaSalta = new Provincia("Salta");
-            Provincia provinciaCorrientes = new Provincia("Corrientes");
-            Provincia nacional = new Provincia("Nacional");
+          context.Provincias.Add(p);
+        }
+        context.SaveChanges();
 
-            PartidoPolitico pro = new PartidoPolitico("Pro", provinciaBsAs);
-            var provincias = new Provincia[] { provinciaBsAs, provinciaCba, provinciaSalta, provinciaCorrientes, nacional };
-            if (!context.Provincias.Any())
-            {
+      }
+      if (!context.Localidades.Any())
+      {
+        context.Localidades.Add(new Localidad("Jose C. Paz", provinciaBsAs));
+        context.Localidades.Add(new Localidad("Merlo", provinciaBsAs));
+        context.Localidades.Add(new Localidad("Rio Cuarto", provinciaCba));
+        context.Localidades.Add(new Localidad("San Miguel", provinciaBsAs));
+        context.Localidades.Add(new Localidad("", provinciaBsAs));
+        context.Localidades.Add(new Localidad("", nacional));
+        context.SaveChanges();
+      }
+      if (!context.Partidos.Any())
+      {
+        context.Partidos.Add(pro);
+        context.SaveChanges();
+      }
+      Candidato presidente = new Candidato("Gaston", "Sturla", context.Localidades.Find(3), Cargo.Concejal, "https://image.freepik.com/vector-gratis/hombre-de-la-cara-de-dibujos-animados_17-1006085726.jpg", pro);
 
-              foreach (Provincia p in provincias)
-              {
-                context.Provincias.Add(p);
-              }
-              context.SaveChanges();
+      if (!context.Candidatos.Any())
+      {
+        Candidato vice = new Candidato("Fernando", "Caveri", context.Localidades.Find(1), Cargo.SenadorNacional, "https://image.freepik.com/vector-gratis/hombre-de-la-cara-de-dibujos-animados_17-1006085726.jpg", pro);
+        context.Candidatos.Add(presidente);
+        context.Candidatos.Add(vice);
+        context.SaveChanges();
+      }
+      Mesa m1 = new Mesa(context.Localidades.Find(2));
+      if (!context.Mesas.Any())
+      {
+        context.Mesas.Add(m1);
+        context.SaveChanges();
+      }
+      if (!context.Recuentos.Any())
+      {
+        Recuento r = new Recuento(presidente, 20, m1);
+        context.Recuentos.Add(r);
+        context.SaveChanges();
+      }
 
-            }
-            if (!context.Localidades.Any())
-            {
-              context.Localidades.Add(new Localidad("Jose C. Paz", provinciaBsAs));
-              context.Localidades.Add(new Localidad("Merlo", provinciaBsAs));
-              context.Localidades.Add(new Localidad("Rio Cuarto", provinciaCba));
-              context.Localidades.Add(new Localidad("San Miguel", provinciaBsAs));
-              context.Localidades.Add(new Localidad("", provinciaBsAs));
-              context.Localidades.Add(new Localidad("", nacional));
-              context.SaveChanges();
-            }
-            if (!context.Partidos.Any())
-            { 
-              context.Partidos.Add(pro);
-              context.SaveChanges();
-            }
-            Candidato presidente = new Candidato("Gaston", "Sturla", context.Localidades.Find(3), Cargo.Concejal, "https://image.freepik.com/vector-gratis/hombre-de-la-cara-de-dibujos-animados_17-1006085726.jpg", pro);
-
-            if (!context.Candidatos.Any())
-            {
-              Candidato vice = new Candidato("Fernando", "Caveri", context.Localidades.Find(1), Cargo.SenadorNacional, "https://image.freepik.com/vector-gratis/hombre-de-la-cara-de-dibujos-animados_17-1006085726.jpg",pro);
-              context.Candidatos.Add(presidente);
-              context.Candidatos.Add(vice);
-              context.SaveChanges();
-            }
-            Mesa m1 = new Mesa(context.Localidades.Find(2));
-            if (!context.Mesas.Any())
-            {
-              context.Mesas.Add(m1);
-              context.SaveChanges();
-            }
-            if (!context.Recuentos.Any())
-            {
-              Recuento r = new Recuento(presidente, 20, m1);
-              context.Recuentos.Add(r);
-              context.SaveChanges();
-            }
-
-
+      context.Usuarios.Add(new Usuario("sadmin", "sadmin", "Super Administrador", 2));
 
 
     }
