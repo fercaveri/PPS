241baa4f084ab099ea8ca92e2bb9ceabd6bcadbc
Gaston <gastonsturla01@gmail.com>
2017-10-09 14:43:35 -0300

    Pantalla de login al principio, y restricciones a la web

--- PPS/src/app/app.component.ts
+++ PPS/src/app/app.component.ts
@@ -9,9 +9,31 @@ export class AppComponent implements OnInit {
     constructor(private _httpService: Http) { }
     apiValues: object[] = [];
     show: string = "";
+    user: String;
+    pass: String;
+    rol: number;
+    showAlert: boolean;
+    logeo : boolean = false;
     ngOnInit() {
         this._httpService.get('/api/provincia').subscribe(values => {
             this.apiValues = values.json() as object[];
         });
+    }
+    login() {
+        this._httpService.get('/api/usuario?usuario=' + this.user + '&pass=' + this.pass).subscribe(values => {
+            this.rol = Number(values.text('legacy'));
+            console.log(this.rol);
+            this.showAlert = true;
+        });
+    }
+    ok() {
+        this.logeo = true;
+    }
+    logout() {
+        this.logeo = false;
+        this.rol = -1;
+        this.showAlert = false;
+        this.user = "";
+        this.pass = "";
     }
 }
